07402ec06971ccf698aa069a25d9f834dd0b3c0a
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2021-05-01 21:38:37 -0300

    COMPLETA

---
+++ exercicios3-estrutura-while/Exe03.java
@@ -0,0 +1,58 @@
+/*
+ 
+ */
+
+
+import java.util.Scanner;
+public class Exe03 {
+	
+	public static void main (String[] args) {
+		
+			
+			Scanner sc = new Scanner (System.in);
+				int a = 0 , g = 0, d = 0;
+				
+				
+				 
+				boolean  continuar = true;
+				
+ 			System.out.println(" ** ESCOLHA O TIPO DE COMBUSTIVEL ** ");
+			System.out.println(" 1 - ALCOOL ");
+			System.out.println(" 2 - GASOLINA");
+			System.out.println(" 3 - DIESEL");
+			System.out.println(" 4 - SAIR");
+		
+		do{
+
+			
+			System.out.print("\nInsira sua opcao: ");
+			int x = sc.nextInt();
+			
+			
+			if ( x == 1){
+				
+				a++;
+				
+				}
+				else if ( x == 2){
+					
+					g++;
+					} 
+					else if ( x == 3){
+					
+					d++;
+					} 
+					else if (x == 4 ){
+						continuar = false;
+                System.out.print(" \n        MUITO OBRIGADA  !!!");
+                System.out.print("\n CLIENTES QUE ESCOLHERAM ALCOOL: "+a);
+                 System.out.print("\n CLIENTES QUE ESCOLHERAM GASOLINA: "+g);
+                  System.out.print("\n CLIENTES QUE ESCOLHERAM DIESEL: "+d);
+						}
+					 
+			} while( continuar );
+			
+			
+	}
+}
+
