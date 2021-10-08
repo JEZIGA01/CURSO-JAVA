5e3e4070f6106f1dc82999080736e375fbd6685b
Jessica Aquiles <jessica.aquiles@hotmail.com>
2021-10-08 00:16:42 -0300

    Exe1

--- POO/POO-EXERCICIOS-04/Exe/src/main/java/Application/Program1.java
+++ POO/POO-EXERCICIOS-04/Exe/src/main/java/Application/Program1.java
@@ -1,36 +1,40 @@
 package Application;
 
-import entities.Exe1;
- 
 import java.util.Scanner;
+
+import entities.Exe1;
+  
 
 public class Program1 {
     public static void main(String[] args) {
 
-        Scanner scs = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+ 
+        String modelo="",marca="",tipo="",estado="";
+ 
 
-        System.out.print("Digite o modelo da lampada: ");
-        String modelo = scs.nextLine();
-        System.out.print("Digite a marca da lampada: ");
-        String marca = scs.nextLine();
-        System.out.print("Digite o tipo da lampada: ");
-        String tipo= scs.nextLine();
-        System.out.print("Digite o estado da lampada: ");
-        String estado = scs.nextLine();
+        Exe1 produto = new Exe1(modelo, marca, tipo, estado); 
+        
+       produto.setModelo("XSDD");
+       produto.setMarca("xuniL");
+       produto.setTipo("44-w");
+       produto.setEstado("desligado");
+       
 
-        scs.close();
-
-        Exe1 produto = new Exe1(modelo,marca,tipo,estado);
-
-        produto.setMarca(modelo);
-        System.out.println("Modelo atualizado: "+produto.getModelo());
-        produto.setMarca(marca);
-        System.out.println("Marca atualizado: "+produto.getMarca());
-        produto.setTipo(tipo);
-        System.out.println("Tipo atualizado: "+produto.getTipo());
-        produto.setEstado(estado);
-        System.out.println("Estado atualizado: "+produto.getEstado());
+       System.out.println("Modelo atualizado: "+produto.getModelo());
+       
+       System.out.println(" Marca atualizada: "+produto.getMarca());
+       
+       System.out.println(" Tipo atualizado: "+produto.getTipo());
+       
+       System.out.println(" Estado atualizado: "+produto.getEstado());
 
 
+
+        
+
+  
+
+       
     }
 }
--- POO/POO-EXERCICIOS-04/Exe/src/main/java/entities/Exe1.java
+++ POO/POO-EXERCICIOS-04/Exe/src/main/java/entities/Exe1.java
@@ -9,12 +9,12 @@ public class Exe1 {
 
     public Exe1(String tipo, String marca, String modelo, String estado){
 
-        this.tipo = tipo;
-        this.estado = estado;
+        this.tipo = tipo; 
         this.marca = marca;
-        this.modelo = modelo;
+        this.modelo = modelo; 
+        this.estado = estado;
     }
-
+      
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
@@ -22,28 +22,33 @@ public class Exe1 {
     public void setMarca(String marca) {
         this.marca = marca;
     }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
+    
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
-    public String getEstado() {
-        return estado;
+
+    public void setEstado(String estado) {
+        this.estado = estado ;
     }
+
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
 
     public String getMarca() {
         return marca;
     }
-
+    
     public String getModelo() {
         return modelo;
     }
 
-    public String getTipo() {
-        return tipo;
+    public String getEstado() {
+       
+        return estado;
     }
+
 }
