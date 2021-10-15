c5245c9cc8f55b2bb01b18381da9d21833ac4026
Jessica Aquiles <jessica.aquiles@hotmail.com>
2021-10-15 14:35:29 -0300

    Boxing , unboxing e wrapper classes

---
+++ exercicios6-memorias-arrays-listas/src/main/java/Examples/Exe3.java
@@ -0,0 +1,18 @@
+package Examples;
+
+public class Exe3 {
+
+    public static void main(String[] args) {
+
+        int x = 20;
+
+       Integer objeto =  x;
+
+        System.out.println(x);
+
+        int y = objeto * 3;
+
+        System.out.println(y);
+
+    }
+}
