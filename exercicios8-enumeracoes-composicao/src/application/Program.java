6e8d8dc25232b2875327de677d9c8329f3ace083
Jéssica Aquiles <73039194+JEZIGA01@users.noreply.github.com>
2022-01-18 09:05:31 -0300

    Enum/Composicao

---
+++ exercicios8-enumeracoes-composicao/src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Order order = new Order (1080, new Date(), OrderStatus.PENDING_PAYMENT);
+				
+				System.out.println(order.toString());
+				
+				OrderStatus os = OrderStatus.DELIVERED;
+				OrderStatus os1 = OrderStatus.valueOf("DELIVERED");
+				
+				System.out.println(os);
+				System.out.println(os1);
+	}
+
+}
