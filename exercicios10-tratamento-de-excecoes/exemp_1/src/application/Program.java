41a3c67471d0c04225681c677aa925a948afcf1b
Jéssica Aquiles <73039194+JEZIGA01@users.noreply.github.com>
2022-02-14 09:43:47 -0300

    try-catch-Finally // File

--- exercicios10-tratamento-de-excecoes/exemp_1/src/application/Program.java
+++ exercicios10-tratamento-de-excecoes/exemp_1/src/application/Program.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+		// Se for no iMac : File file = new File("/Users/user/desktop/in.txt");
 		File file = new File("C:\\temp\\in.txt");
 		Scanner sc = null;
 		try {
