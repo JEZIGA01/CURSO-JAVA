b7c79f7cbea9167baaa1fba42f8964bda104bc3f
Jéssica <73039194+JEZIGA01@users.noreply.github.com>
2022-05-10 10:08:46 -0300

    AULA JPA

---
+++ AulaJPA/src/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa(1,"Jessica Fernanda", "fernanda@gmail.com");
+		Pessoa p2 = new Pessoa(1,"Jessica Aquiles", "aquiles@gmail.com");
+		Pessoa p3 = new Pessoa(1,"Jessica Lucio", "lucio@gmail.com");
+		
+	}
+
+}
